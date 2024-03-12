566d09dd1015ad93f47fbd9598a9150abb5b4237
Vaibhav Gangil <vaibhav.gangil@vaibhavgangil-MacBook-Pro.local>
2024-03-13 01:02:55 +0530

    v1 for DNS Manager

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './DashBoard/DashBoard';
+import AddDNSForm from './Forms/AddDNSForm';
+import EditDNSForm from './Forms/EditDNSForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={
+            <div>
+              <Dashboard/>
+            </div>
+        } 
+        />
+        <Route path="/newDNS" element={
+            <div>
+              <AddDNSForm/>
+            </div>
+        } />
+        <Route path={`/editDNS/:id`} element={
+          <div>
+            <EditDNSForm/>
+          </div>
+        }/>
+      </Routes>
+    </Router>
   );
 }
 
